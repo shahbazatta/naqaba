e3fa47a50202e241dbecfd7081bc42f9c1b23464
umair <dfgfdg>
2023-05-12 23:01:54 +0500

    draw control added

--- assets/js/gis/main.js
+++ assets/js/gis/main.js
@@ -16,45 +16,83 @@ var basemapLayer = new ol.layer.Tile({
 map.addLayer(basemapLayer);
 var busesLyr;
 var busesDataSource;
+var drawSource = new ol.source.Vector();
+var draw;
 
+//use this funtion on draw geofence button click
+function toggleDrawGeofenceCtrl()
+	{
+		draw.setActive(!draw.getActive())
+	}
+function addDrawInteraction()
+	{
+		var drawLayer = new ol.layer.Vector({
+		  source: drawSource
+		});
+
+		// Add the vector layer to the map
+		map.addLayer(drawLayer);
+
+
+		// Create a draw interaction for polygons
+		draw = new ol.interaction.Draw({
+		  source: drawSource,
+		  type: 'Polygon'
+		});
+
+		// Event listener for drawstart event
+		draw.on('drawstart', function(event) {
+		  console.log('Polygon drawing started');
+		});
+
+		// Event listener for drawend event
+		draw.on('drawend', function(event) {
+		  var polygon = event.feature.getGeometry();
+		  console.log('Polygon drawing ended:', polygon.getCoordinates());
+		  // Do something with the drawn polygon geometry
+		});
+
+		// Add the draw interaction to the map
+		map.addInteraction(draw);
+	}
 function getAllBusesData()  
-{
-	//ajax call to api get all bus data
-	$.ajax({
-	  url: 'https://tracking.naqaba.com.sa/api/getDevicesDataLive?token=cebc8011932a85c60a7e079b840bf083161812d3&min=10',
-	  method: 'GET',
-	  dataType: 'json',
-	  success: function(response) {
-		var dataArr = response;
-		var featuresArr =[]
-		for (let i = 0; i < dataArr.length; i++) {
-		var obj = dataArr[i];
-		var feature = new ol.Feature({geometry: new ol.geom.Point(ol.proj.fromLonLat(obj.location.coordinates))})
-		var iconStyle = new ol.style.Style({
-			image: new ol.style.Icon({
-			  src: 'assets/images/bus.png', // Replace with the path to your bus icon image
-			  scale: 0.05// Adjust the scale as needed
-			})
-		  });
+	{
+		//ajax call to api get all bus data
+		$.ajax({
+		  url: 'https://tracking.naqaba.com.sa/api/getDevicesDataLive?token=cebc8011932a85c60a7e079b840bf083161812d3&min=10',
+		  method: 'GET',
+		  dataType: 'json',
+		  success: function(response) {
+			var dataArr = response;
+			var featuresArr =[]
+			for (let i = 0; i < dataArr.length; i++) {
+			var obj = dataArr[i];
+			var feature = new ol.Feature({geometry: new ol.geom.Point(ol.proj.fromLonLat(obj.location.coordinates))})
+			var iconStyle = new ol.style.Style({
+				image: new ol.style.Icon({
+				  src: 'assets/images/bus.png', // Replace with the path to your bus icon image
+				  scale: 0.05// Adjust the scale as needed
+				})
+			  });
 
-		  // Set the icon style for the feature
-		  feature.setStyle(iconStyle);
-		featuresArr.push(feature);
-		}
-		 
-		busesDataSource = new ol.source.Vector({
-				 features: featuresArr
-			 })
-		 busesLyr = new ol.layer.Vector({
-			 source: busesDataSource
-		 });
-		map.addLayer(busesLyr);
-	  },
-	  error: function(xhr, status, error) {
-		console.log('Error:', error);
-	  }
-	});
-}
+			  // Set the icon style for the feature
+			  feature.setStyle(iconStyle);
+			featuresArr.push(feature);
+			}
+			 
+			busesDataSource = new ol.source.Vector({
+					 features: featuresArr
+				 })
+			 busesLyr = new ol.layer.Vector({
+				 source: busesDataSource
+			 });
+			map.addLayer(busesLyr);
+		  },
+		  error: function(xhr, status, error) {
+			console.log('Error:', error);
+		  }
+		});
+	}
 
 getAllBusesData()
 
