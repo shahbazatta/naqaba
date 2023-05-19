7d2a270c80e71462466699875af4d9193b668111
umair <dfgfdg>
2023-05-20 02:23:06 +0500

    bug fixed

--- assets/js/gis/main.js
+++ assets/js/gis/main.js
@@ -188,13 +188,14 @@ function getAllGeofence()
 				  geometry:polygon,
 				  properties: obj
 				});
-				feature.setId(obj._id);
+				//feature.setId(obj._id);
 				stationArr.push(feature);
 			  }
-			  var stationSource = new ol.source.Vector(
-			  {
-				  		features: stationArr
-			  }
+			  
+			  
+			  var stationSource = new ol.source.Vector({
+											features: stationArr
+											});
 			  );
 				var stationStyle = new ol.style.Style({
 					stroke: new ol.style.Stroke({
@@ -212,7 +213,7 @@ function getAllGeofence()
 				  });
 			  
 			  map.addLayer(stationLyr);
-			    map.getView().fit(stationSource.getExtent());
+			  map.getView().fit(stationSource.getExtent());
 
 			 },
          error: function (jqXHR, exception) {
