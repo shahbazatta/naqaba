7ee435ff8fef59ce8193e71a1908219f63e6d4fd
umair <dfgfdg>
2023-05-19 12:42:09 +0500

    get geofnce added

--- assets/js/gis/main.js
+++ assets/js/gis/main.js
@@ -165,13 +165,53 @@ error: function(xhr, status, error) {
 }
 });
 }
+
+function getAllGeofence()
+{
+	
+    $.ajax({
+         url: "data/get_geofence.php",
+         method: "POST",
+         dataType: "json",
+        data: {
+          api_key: "becdf4fbbbf49dbc",
+         },
+         success: function(data){console.log(data);},
+         error: function (jqXHR, exception) {
+           var msg = '';
+          //alert(msg);
+           console.log(jqXHR.responseText);
+
+           if (jqXHR.status === 0) {
+             msg = 'Not connect.\n Verify Network.';
+           } else if (jqXHR.status == 404) {
+             msg = 'Requested page not found. [404]';
+           } else if (jqXHR.status == 500) {
+             msg = 'Internal Server Error [500].';
+           } else if (exception === 'parsererror') {
+             msg = 'Requested JSON parse failed.';
+           } else if (exception === 'timeout') {
+             msg = 'Time out error.';
+           } else if (exception === 'abort') {
+             msg = 'Ajax request aborted.';
+           } else {
+             msg = 'Uncaught Error.\n' + jqXHR.responseText;
+           }
+           alert(msg);
+         },
+     });
+}
+
 addDrawInteraction();
 getAllBusesData();
+getAllGeofence();
 document.getElementById("draw_geofence").addEventListener("click", toggleDrawGeofenceCtrl);
 
  document.getElementById("bmap").onchange = function(){
 					switchBaseMaps();
  };
+ 
+
 
 
 setInterval(getAllBusesData, 240 * 1000); //api call after every 4 minutes
