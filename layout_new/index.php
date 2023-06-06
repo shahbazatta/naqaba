2df7c540bb0766f71be73babfd0c54b5558b84b3
khurram <miankhurram9797@gmail.com>
2023-06-06 23:49:35 +0500

    minor bug in code

--- layout_new/index.php
+++ layout_new/index.php
@@ -573,7 +573,7 @@ require_once("lang/language.php");
           <td colspan="1"><?php echo $localizedStrings->String($localizedStrings::LC_EN, 'OperatorNo'); ?>
             <span id ="operator_no">1685387738376</span>
           </td>
-          <td colspan="1"><?php echo $localizedStrings->String($localizedStrings::LC_EN, 'Engine Plate No'); ?>
+          <td colspan="1"><?php echo $localizedStrings->String($localizedStrings::LC_EN, 'EnginePlateNo'); ?>
             <span id ="engplate_no">6474f9da97c5bc121663c1a9</span>
           </td>
         </tr>
