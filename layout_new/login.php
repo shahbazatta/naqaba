01acb4c18055b16f35b2d7b418497062cdea60ec
Waqas Siddiqui <waqas.ninetytwo@gmail.com>
2023-06-06 23:46:25 +0500

    Layout Direction set

--- layout_new/login.php
+++ layout_new/login.php
@@ -7,7 +7,11 @@ require_once("lang/language.php");
 <!--[if IE 9]>          <html class="ie ie9"> <![endif]-->
 <!--[if gt IE 9]><!-->
 
+<?php if ($lang_type == 'ar'){ ?>
+<html lang="ar" dir="rtl" data-textdirection="rtl">
+<?php } else { ?>
 <html lang="en" dir="ltr" data-textdirection="ltr">
+<?php } ?>
 
 <head>
 <!-- Basic -->
@@ -17,6 +21,7 @@ require_once("lang/language.php");
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Noto+Kufi+Arabic&display=swap" rel="stylesheet">
 
 <!-- jQuery -->
 <script src="assets/js/jquery-1.11.2.min.js"></script>
@@ -24,6 +29,10 @@ require_once("lang/language.php");
 <!-- Styling -->
 <link rel="stylesheet" href="assets/css/reset.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="assets/css/style.css" type="text/css" media="screen" />
+
+<?php if ($lang_type == 'ar'){ ?>
+<link rel="stylesheet" href="assets/css/style_arabic.css" type="text/css" media="screen" />
+<?php } ?>
 
 <!--[if lt IE 9]>
   <script src="assets/js/html5.js"></script>
