a537947b75caa34ed5ffb756157da95e22411d7f
Waqas Siddiqui <waqas.ninetytwo@gmail.com>
2023-07-09 12:02:05 +0500

    chart page

---
+++ layout_new/chart.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--[if IE 8]>          <html class="ie ie8"> <![endif]-->
+<!--[if IE 9]>          <html class="ie ie9"> <![endif]-->
+<!--[if gt IE 9]><!-->
+<html lang="en" dir="ltr" data-textdirection="ltr">
+<head>
+<!-- Basic -->
+<meta charset="utf-8">
+<title><?php echo $localizedStrings->String($localizedStrings::LC_EN, 'naqabahTrackerSystemv1'); ?></title>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Prompt:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+<!-- jQuery -->
+<script src="assets/js/jquery-3.6.0.min.js"></script>
+<!-- Styling -->
+<link rel="stylesheet" href="assets/css/reset.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="assets/css/style.css" type="text/css" media="screen" />
+<!--[if lt IE 9]>
+	<script src="assets/js/html5.js"></script>
+	<link rel="stylesheet" href="assets/css/ie.css">
+<![endif]-->
+</head>
+<body>
+
+<div class="chartReport" id="chartReport"></div>
+
+</body>
+</html>
+
