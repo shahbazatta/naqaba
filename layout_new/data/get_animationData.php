bb69af4149a46e7938f8f56ddea4e1a23e049817
shahbazatta <shahbaz.atta@gmail.com>
2023-07-13 13:52:14 +0500

    new collection updated

--- layout_new/data/get_animationData.php
+++ layout_new/data/get_animationData.php
@@ -39,7 +39,7 @@ if( isset($_POST["api_key"]) && isset($_POST["imei_no"]) && isset($_POST["start_
 
 
 		$db = $client->selectDatabase(DB_NAME);
-		$collection = $db->gpsLive;
+		$collection = $db->gpsHistorical;
 		//$cursor = $collection->find(array('imei' => $imei_rec));
 		$cursor = $collection->find(array('avltm'=>array ('$gte'=> (int) $gt_date2, '$lte' => (int) $lt_date2 ), 'imei' => $imei_rec));
 
