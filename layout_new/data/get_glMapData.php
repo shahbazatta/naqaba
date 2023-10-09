deb7369b9d404a340803e8693ce3b29378f134ae
ans1 <ans2003@gmail.com>
2023-10-09 21:03:40 +0500

    Bus animation data load with clickhouse

--- layout_new/data/get_glMapData.php
+++ layout_new/data/get_glMapData.php
@@ -2,63 +2,64 @@
 require_once("../config/config.php");
 require_once '../vendor/autoload.php';
 
-use MongoDB\Exception;
+//use MongoDB\Exception;
+//use MongoDB\BSON\UTCDateTime;
 use MongoDB\Client;
+use ClickHouseDB\Exception;
 
-if( isset($_POST["api_key"]) && isset($_POST["imei_no"]) && isset($_POST["start_date"]) && isset($_POST["end_date"]) ) {
-	if (empty($_POST["api_key"]) || empty($_POST["imei_no"]) || empty($_POST["start_date"]) || empty($_POST["end_date"]) || $_POST["api_key"] != 'becdf4fbbbf49dbc') {
+if (isset($_POST["api_key"]) && isset($_POST["imei_no"]) && isset($_POST["start_date"]) && isset($_POST["end_date"])) {
+    if (empty($_POST["api_key"]) || empty($_POST["imei_no"]) || empty($_POST["start_date"]) || empty($_POST["end_date"]) || $_POST["api_key"] != 'becdf4fbbbf49dbc') {
 
-	    echo '{"Error":"Invalid or wrong value"}';
-	}
-	else{
+        echo '{"Error":"Invalid or wrong value"}';
+    } else {
 
-		//Atlas connection string
-		//$uri = 'mongodb://shahbaz:Islam786ian@209.38.216.19:27017/';
+        //Atlas connection string
+        //$uri = 'mongodb://shahbaz:Islam786ian@209.38.216.19:27017/';
 
-		// Create a new client and connect to the server
-		$client = new \MongoDB\Client(DB_SERVER_URL);
-		//$client = new \MongoDB\Client(CONNECTION_STRING );
+        $config = [
+            'host' => '209.38.216.19',
+            'port' => '8123',
+            'username' => 'default',
+            'password' => 'Islam786ian'
+        ];
 
-		try {
-		    // Send a ping to confirm a successful connection
-		    $client->selectDatabase(DB_NAME)->command(['ping' => 1]);
-		    //echo "Pinged your deployment. You successfully connected to MongoDB! <br>";
-		} catch (Exception $e) {
-		    printf($e->getMessage());
-		}
+        // Create a new client and connect to the server
+        $client = new ClickHouseDB\Client($config);
+        //$db = $client->database(DB_NAME);
 
-		$imei_rec = (int)trim($_POST["imei_no"]);
-		$start_date = trim($_POST["start_date"]);
-		$end_date = trim($_POST["end_date"]);
-
-        //return $imei_rec.' '.$start_date.' '.$end_date;
-
-		$gt_date = new \MongoDB\BSON\UTCDateTime(strtotime($start_date) * 1000);
-		$lt_date = new \MongoDB\BSON\UTCDateTime(strtotime($end_date) * 1000);
-
-		$gt_date2 = (string) $gt_date;
-		$lt_date2 = (string) $lt_date;
+        try {
+            // Send a ping to confirm a successful connection
+            $client->database(DB_NAME);
+            //$client->ping(true);
+            //echo "Pinged your deployment. You successfully connected to CLick House! <br>";
+        } catch (Exception $e) {
+            printf($e->getMessage());
+        }
 
 
-		$db = $client->selectDatabase(DB_NAME);
-		$collection = $db->gpsHistorical;
-		// $cursor = $collection->find(array('imei' => $imei_rec));
-		$cursor = $collection->find(
-            array(
-            'avltm'=>array ('$gte'=> (int) $gt_date2, '$lte' => (int) $lt_date2 ),
-            'spd'=>array ('$gt'=> 0),
-            'imei' => $imei_rec
-        ), ['sort' => ['avltm' => 1], 'limit' => 1000] );
+        $imei_rec = (int)trim($_POST["imei_no"]);
+        $start_date = new DateTime(trim($_POST["start_date"]), new DateTimeZone('UTC'));
+        $end_date = new DateTime(trim($_POST["end_date"]), new DateTimeZone('UTC'));
+
+        $gt_date = $start_date->format('Y-m-d H:i:s');
+        $lt_date = $end_date->format('Y-m-d H:i:s');
+
+
+        $db = $client->database(DB_NAME);
+        //$collection = $db->gpsHistorical;
+        // $cursor = $collection->find(array('imei' => $imei_rec));
+//        $cursor = $collection->find(
+//            array(
+//                'avltm' => array('$gte' => (int)$gt_date2, '$lte' => (int)$lt_date2),
+//                'spd' => array('$gt' => 0),
+//                'imei' => $imei_rec
+//            ), ['sort' => ['avltm' => 1], 'limit' => 1000]);
+
+        //$cursor = $db->select("SELECT * FROM gpsHistorical WHERE avltm >= '$gt_date' AND avltm <= '$lt_date' AND spd > 0 AND imei = '$imei_rec' ORDER BY avltm DESC LIMIT 5");
+        $cursor = $db->select("SELECT * FROM gpsHistorical WHERE imei = '$imei_rec' AND avltm >= '$gt_date' AND avltm <= '$lt_date'  spd > 0 ORDER BY avltm DESC LIMIT 1000");
 
         $json_data = null;
-
-        //print_r($cursor);
-
-        //echo $json_data = json_encode(iterator_to_array($cursor), JSON_UNESCAPED_UNICODE);
-
         $cursor = iterator_to_array($cursor);
-
-        //if ($cursor)
 
         $new_cursor = array();
         $times = [];
@@ -70,21 +71,38 @@ if( isset($_POST["api_key"]) && isset($_POST["imei_no"]) && isset($_POST["start_
         foreach ($cursor as $key => $item) {
 //            $new_cursor['coordinates'] = $item['location']['coordinates'];
 //            $new_cursor['times'] = $item['avltm'];
-            $coordinates[$key] = $item['location']['coordinates'];
+            $coordinates[$key] = $item['location'];
             $times[$key] = $item['avltm'];
             $arrCounter++; // increment
         }
 
-        $firstTimestamp = $times[0] / 1000;
-        $relativeTimes = array_map(function ($timestamp) use ($firstTimestamp) {
-            return round(($timestamp / 1000 - $firstTimestamp) / 60);
+        // Step 1: Convert timestamps to Unix timestamps (seconds since epoch)
+        $timestampObjects = array_map(function ($timestamp) {
+            return strtotime($timestamp);
         }, $times);
 
-        $new_cursor = ['coordinates' => $coordinates, 'relTimes' => $relativeTimes, 'avltm' => $times, 'arrCounter' => $arrCounter, 'imei' => $imei];
+        // Step 2: Calculate time differences in seconds
+        $timeDifferences = array_map(function ($timestamp, $firstTimestamp) {
+            return $timestamp - $firstTimestamp;
+        }, $timestampObjects, [$timestampObjects[0]]);
 
-		echo $json_data = json_encode($new_cursor, JSON_UNESCAPED_UNICODE);
-	}
+        // Step 3: Convert time differences to minutes
+        $minutesDifferences = array_map(function ($timeDifference) {
+            return floor($timeDifference / 60);
+        }, $timeDifferences);
 
-}else{
-	 header("Location: ../index.php");
+        // Optionally, you can generate the timesArr array with a specified interval
+        $interval = 6; // Set your desired interval in minutes
+        $timesArr = range(0, (count($times) - 1) * $interval, $interval);
+
+        $new_cursor = ['coordinates' => $coordinates, 'relTimes' => $timesArr, 'avltm' => $times, 'arrCounter' => $arrCounter, 'imei' => $imei];
+
+//        print_r($new_cursor);
+//        exit;
+
+        echo $json_data = json_encode($new_cursor, JSON_UNESCAPED_UNICODE);
+    }
+
+} else {
+    header("Location: ../index.php");
 }
---
+++ layout_new/data/get_glMapData_bk.php
@@ -0,0 +1,90 @@
+<?php
+require_once("../config/config.php");
+require_once '../vendor/autoload.php';
+
+use MongoDB\Exception;
+use MongoDB\Client;
+
+if( isset($_POST["api_key"]) && isset($_POST["imei_no"]) && isset($_POST["start_date"]) && isset($_POST["end_date"]) ) {
+	if (empty($_POST["api_key"]) || empty($_POST["imei_no"]) || empty($_POST["start_date"]) || empty($_POST["end_date"]) || $_POST["api_key"] != 'becdf4fbbbf49dbc') {
+
+	    echo '{"Error":"Invalid or wrong value"}';
+	}
+	else{
+
+		//Atlas connection string
+		//$uri = 'mongodb://shahbaz:Islam786ian@209.38.216.19:27017/';
+
+		// Create a new client and connect to the server
+		$client = new \MongoDB\Client(DB_SERVER_URL);
+		//$client = new \MongoDB\Client(CONNECTION_STRING );
+
+		try {
+		    // Send a ping to confirm a successful connection
+		    $client->selectDatabase(DB_NAME)->command(['ping' => 1]);
+		    //echo "Pinged your deployment. You successfully connected to MongoDB! <br>";
+		} catch (Exception $e) {
+		    printf($e->getMessage());
+		}
+
+		$imei_rec = (int)trim($_POST["imei_no"]);
+		$start_date = trim($_POST["start_date"]);
+		$end_date = trim($_POST["end_date"]);
+
+        //return $imei_rec.' '.$start_date.' '.$end_date;
+
+		$gt_date = new \MongoDB\BSON\UTCDateTime(strtotime($start_date) * 1000);
+		$lt_date = new \MongoDB\BSON\UTCDateTime(strtotime($end_date) * 1000);
+
+		$gt_date2 = (string) $gt_date;
+		$lt_date2 = (string) $lt_date;
+
+
+		$db = $client->selectDatabase(DB_NAME);
+		$collection = $db->gpsHistorical;
+		// $cursor = $collection->find(array('imei' => $imei_rec));
+		$cursor = $collection->find(
+            array(
+            'avltm'=>array ('$gte'=> (int) $gt_date2, '$lte' => (int) $lt_date2 ),
+            'spd'=>array ('$gt'=> 0),
+            'imei' => $imei_rec
+        ), ['sort' => ['avltm' => 1], 'limit' => 1000] );
+
+        $json_data = null;
+
+        //print_r($cursor);
+
+        //echo $json_data = json_encode(iterator_to_array($cursor), JSON_UNESCAPED_UNICODE);
+
+        $cursor = iterator_to_array($cursor);
+
+        //if ($cursor)
+
+        $new_cursor = array();
+        $times = [];
+        $coordinates = [];
+        $arrCounter = 0;
+        $imei = $cursor[0]['imei'];
+
+
+        foreach ($cursor as $key => $item) {
+//            $new_cursor['coordinates'] = $item['location']['coordinates'];
+//            $new_cursor['times'] = $item['avltm'];
+            $coordinates[$key] = $item['location']['coordinates'];
+            $times[$key] = $item['avltm'];
+            $arrCounter++; // increment
+        }
+
+        $firstTimestamp = $times[0] / 1000;
+        $relativeTimes = array_map(function ($timestamp) use ($firstTimestamp) {
+            return round(($timestamp / 1000 - $firstTimestamp) / 60);
+        }, $times);
+
+        $new_cursor = ['coordinates' => $coordinates, 'relTimes' => $relativeTimes, 'avltm' => $times, 'arrCounter' => $arrCounter, 'imei' => $imei];
+
+		echo $json_data = json_encode($new_cursor, JSON_UNESCAPED_UNICODE);
+	}
+
+}else{
+	 header("Location: ../index.php");
+}
